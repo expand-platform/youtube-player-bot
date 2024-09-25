768d61859f3a8f43052b2b273b503ea6c49c153d
DL <damir96.lukyanenko@gmail.com>
2024-09-25 20:41:25 +0300

    add new students

--- src/users/students.py
+++ src/users/students.py
@@ -4,5 +4,17 @@ STUDENTS = [
         "real_name": "Оля",
         "payment_amount": 2500,
         "payment_status": False,
-    }
+    },
+    {
+        "user_id": 782692408,
+        "real_name": "Кирилл",
+        "payment_amount": 3500,
+        "payment_status": False,
+    },
+    {
+        "user_id": 549683719,
+        "real_name": "Ярослав",
+        "payment_amount": 3300,
+        "payment_status": False,
+    },
 ]
